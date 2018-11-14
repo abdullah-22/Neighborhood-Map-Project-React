8e192b0d42587f40a0b6b5198fbe914d07da6877
Abdullah A. Salman <abdullahahmadsalman@gmail.com>
2018-11-15 00:34:07 +0500

    Updates

---
+++ precache-manifest.eaad7c304b46e4125cfb55c0193fb073.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b82e963cd2c9f04a016d8ad4c464a13a",
+    "url": "/Neighborhood-Map-Project-React/static/media/powered-by-foursquare-blue.b82e963c.svg"
+  },
+  {
+    "revision": "5291900a712187e38413",
+    "url": "/Neighborhood-Map-Project-React/static/js/runtime~main.5291900a.js"
+  },
+  {
+    "revision": "fdca02ebaa22bc8e167c",
+    "url": "/Neighborhood-Map-Project-React/static/js/main.fdca02eb.chunk.js"
+  },
+  {
+    "revision": "1d6ae327e7baf0de86d6",
+    "url": "/Neighborhood-Map-Project-React/static/js/1.1d6ae327.chunk.js"
+  },
+  {
+    "revision": "fdca02ebaa22bc8e167c",
+    "url": "/Neighborhood-Map-Project-React/static/css/main.96148870.chunk.css"
+  },
+  {
+    "revision": "bc6ac11cd49b61e8450d0fdc6876abd2",
+    "url": "/Neighborhood-Map-Project-React/index.html"
+  }
+];
